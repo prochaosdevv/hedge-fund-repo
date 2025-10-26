83671abf1161d12c7bc6e47810531ee743cecc70
prochaosdevv <info@himotechglobal.com>
2025-10-26 20:38:48 +0530

    dd

--- hedgefund-api/cron.js
+++ hedgefund-api/cron.js
@@ -1,7 +1,6 @@
 
 const fs =  require("fs");
-const path = require("path");
-const cron = require("node-cron");
+const path = require("path"); 
 const axios = require("axios");
 const  { fileURLToPath } = require("url");
 const dotenv = require("dotenv");
